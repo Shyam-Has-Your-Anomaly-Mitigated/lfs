0386ebdb0459410cce75a5f2fbeec8fa943b9ae1
Shyam-Has-Your-Anomaly-Mitigated <shyam@shyam.id.au>
2018-04-03 23:30:58 +1000

    a(scii) picture is worth a thousand words

---
+++ chronochron.pl
@@ -0,0 +1,11 @@
+'
+#│started tasks in progress│@
+═╪═════════════════════════╪═══════════════════════
+1│task a                   │YYYY-MM-DD HH:mm:SS.sss
+2│task b                   │YYYY-MM-DD HH:mm:SS.sss
+3│task c                   │YYYY-MM-DD HH:mm:SS.sss
+
+start a new task?
+stop/pause a task?
+complete a task?
+'

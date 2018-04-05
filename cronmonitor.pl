3a2d32b6f6946a1cd5f9a1a79680a33a8dac13db
Shyam-Has-Your-Anomaly-Mitigated <shyam@shyam.id.au>
2018-04-06 04:44:43 +1000

    m

--- cronmonitor.pl
+++ cronmonitor.pl
@@ -1,11 +1,21 @@
 '
+#│paused tasks│@
+═╪════════════╪═══════════════════════
+1│task a      │YYYY-MM-DD HH:mm:SS.sss
+2│task b      │YYYY-MM-DD HH:mm:SS.sss
+3│task c      │YYYY-MM-DD HH:mm:SS.sss
+
 #│started tasks in progress│@
 ═╪═════════════════════════╪═══════════════════════
 1│task a                   │YYYY-MM-DD HH:mm:SS.sss
 2│task b                   │YYYY-MM-DD HH:mm:SS.sss
 3│task c                   │YYYY-MM-DD HH:mm:SS.sss
 
+...
+
 start a new task?
-stop/pause a task?
+stop a task?
+pause a task?
+restart a task?
 complete a task?
 '
